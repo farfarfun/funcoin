85579564a6e130ffb7047a35f7f86a0e95e959b6
niuliangtao <niuliangtao@qq.com>
2023-08-23 21:11:42 +0800

    add

--- funcoin/task/load.py
+++ funcoin/task/load.py
@@ -3,26 +3,28 @@ from datetime import datetime, timedelta
 from ccxt import binance
 from funcoin.coins.base.file import DataFileProperty
 from funcoin.task import BaseTask
-
+from funsecret import read_secret
 
 class LoadTask(BaseTask):
-    table_name = 'load'
+    table_name = "load"
 
     def __init__(self, *args, **kwargs):
-        self.path_root = '/home/bingtao/workspace/tmp'
+        self.path_root = (
+            read_secret(cate1="funcoin", cate2="path", cate3="local", cate4="path_root") or "~/workspace/tmp"
+        )
         super(LoadTask, self).__init__(*args, **kwargs)
 
     @staticmethod
-    def parse_day(ds,days=0):
+    def parse_day(ds, days=0):
         ds = ds
-        first = datetime.strptime(ds, '%Y-%m-%d') - timedelta(days=days)
+        first = datetime.strptime(ds, "%Y-%m-%d") - timedelta(days=days)
         last = first + timedelta(days=1) - timedelta(seconds=1)
         return first, last
 
     @staticmethod
     def parse_week(ds, weeks=0):
         ds = ds
-        first = datetime.strptime(ds, '%Y-%m-%d') + timedelta(weeks=weeks)
+        first = datetime.strptime(ds, "%Y-%m-%d") + timedelta(weeks=weeks)
         first = first - timedelta(days=first.weekday())
         first = datetime(first.year, first.month, first.day)
         last = first + timedelta(weeks=1) - timedelta(seconds=1)
@@ -33,10 +35,10 @@ class LoadKlineDailyTask(LoadTask):
     def refresh(self, ds):
         start, end = self.parse_day(ds)
         file_pro = DataFileProperty(exchange=binance(), path=self.path_root)
-        file_pro.file_format = '%Y%m%d'
-        file_pro.change_data_type('kline')
-        file_pro.change_timeframe('1m')
-        file_pro.change_freq('daily')
+        file_pro.file_format = "%Y%m%d"
+        file_pro.change_data_type("kline")
+        file_pro.change_timeframe("1m")
+        file_pro.change_freq("daily")
         file_pro.load_daily(start, end)
 
 
@@ -44,10 +46,10 @@ class LoadKlineWeeklyTask(LoadTask):
     def refresh(self, ds):
         start, end = self.parse_week(ds, weeks=-1)
         file_pro = DataFileProperty(exchange=binance(), path=self.path_root)
-        file_pro.file_format = '%Y%m%d'
-        file_pro.change_data_type('kline')
-        file_pro.change_timeframe('1m')
-        file_pro.change_freq('weekly')
+        file_pro.file_format = "%Y%m%d"
+        file_pro.change_data_type("kline")
+        file_pro.change_timeframe("1m")
+        file_pro.change_freq("weekly")
         print((start, end))
         file_pro.load_merge(start, end)
 
@@ -56,11 +58,11 @@ class LoadTradeDailyTask(LoadTask):
     def refresh(self, ds):
         start, end = self.parse_day(ds)
         file_pro = DataFileProperty(exchange=binance(), path=self.path_root)
-        file_pro.file_format = '%Y%m%d'
-        file_pro.change_data_type('trade')
-        file_pro.change_timeframe('detail')
-        file_pro.change_freq('daily')
+        file_pro.file_format = "%Y%m%d"
+        file_pro.change_data_type("trade")
+        file_pro.change_timeframe("detail")
+        file_pro.change_freq("daily")
         file_pro.load_daily(start, end)
 
 
-#LoadKlineWeeklyTask().refresh('2021-9-14')
+# LoadKlineWeeklyTask().refresh('2021-9-14')
