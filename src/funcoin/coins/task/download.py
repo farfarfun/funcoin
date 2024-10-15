8cd9f68778caa000d135c71fec2036d58d1373c3
farfun <1007530194@qq.com>
2024-10-16 02:02:22 +0800

    add

--- src/funcoin/coins/task/download.py
+++ src/funcoin/coins/task/download.py
@@ -6,7 +6,7 @@ from funsecret import read_secret
 from funcoin.coins.table.load import LoadTask
 
 
-def download_daily():
+def download_daily(days=365, *arge, **kwargs):
     exchange = ccxt.binance(  # noqa: F821
         {
             "apiKey": read_secret("coin", "binance", "api_key"),
@@ -18,4 +18,4 @@ def download_daily():
     drive.login()
     table = DriveTable(table_fid="66bf1a293f124e5a2d2841eeb0fbb21b70ddb0ee", drive=drive)
     task = LoadTask(table=table, exchange=exchange)
-    task.run()
+    task.run(days=days)
