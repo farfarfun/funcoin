e77326962a498a159720bf32d80e15604c398e8b
farfun <1007530194@qq.com>
2024-10-22 22:53:43 +0800

    add

--- src/funcoin/__init__.py
+++ src/funcoin/__init__.py
@@ -3,3 +3,4 @@ import logging
 logger = logging.getLogger("funcoin")
 logger.setLevel(level=logging.INFO)
 logger.addHandler(logging.StreamHandler())
+logger.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
